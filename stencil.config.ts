b3c4d7bc5a917649224b7cf597953470713e5f40
Andres Mazzo <amazzoccone@bondacom.com>
2019-06-19 18:11:59 -0300

    implement postcss and mixins

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,10 +1,30 @@
 import { Config } from '@stencil/core';
+import { postcss } from '@stencil/postcss';
 
 // https://stenciljs.com/docs/config
 
 export const config: Config = {
   globalStyle: 'src/global/app.css',
   globalScript: 'src/global/app.ts',
+  plugins: [
+    postcss({
+      plugins: [
+        require('postcss-import')(),
+        require('postcss-custom-properties')(),
+        require('postcss-mixins')(),
+        require('postcss-nested')(),
+        require('postcss-simple-vars')(),
+        require('autoprefixer')({
+          browsers: [
+            '>1%',
+            'last 4 versions',
+            'Firefox ESR',
+            'not ie < 9'
+          ]
+        })
+      ]
+    })
+  ],
   outputTargets: [
     {
       type: 'www',
